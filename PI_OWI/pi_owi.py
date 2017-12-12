ee030aaa6a531ff840a56575253dc01cd257289e
Aurash Norouzi <aurashn@users.noreply.github.com>
2017-12-12 16:00:52 -0600

    Add files via upload

--- PI_OWI/pi_owi.py
+++ PI_OWI/pi_owi.py
@@ -3,9 +3,14 @@ from flask import render_template, request
 from flask_cors import CORS, cross_origin
 import time
 
+#create a flask app for hosting web controller
 app = Flask(__name__)
 #CORS(app,resources={r"/": {"origins": "http://10.0.0.10:8081"}})
+
+#create a serial link to the arduino for sending commands 
 import serial
+
+#serial link on ACM0 at 9600 baud rate
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 
@@ -15,54 +20,63 @@ a=1
 def index():
     return render_template('robot.html')
 
-@app.route('/left_side')
+#rotate robot left
+@app.route('/left')
 def left_side():
     data1="LEFT"
     ser.write('l')
     return 'true'
 
-@app.route('/right_side')
+#rotate robot right
+@app.route('/right')
 def right_side():
    data1="RIGHT"
    ser.write('r')
    return 'true'
 
-@app.route('/close_side')
+#close robot gripper
+@app.route('/close')
 def close_side():
    data1="close"
    ser.write('c')
    return 'true'
 
-@app.route('/cup_side')
+#rotate gripper upwards
+@app.route('/cup')
 def cup_side():
    data1="close"
    ser.write('f')
    return 'true'
 
-@app.route('/cd_side')
+#rotate gripper downwards
+@app.route('/cd')
 def cd_side():
    data1="close"
    ser.write('a')
    return 'true'
 
-@app.route('/open_side')
+#open robot gripper
+@app.route('/open')
 def open_side():
    data1="open"
    ser.write('o')
    return 'true'
 
-@app.route('/up_side')
+#rotate robot elbow up
+@app.route('/up')
 def up_side():
    data1="FORWARD"
    ser.write('u')
    return 'true'
 
-@app.route('/down_side')
+#rotate robot elbow down
+@app.route('/down')
 def down_side():
    data1="BACK"
    ser.write('d')
    return 'true'
 
+#stop all movement
 @app.route('/stop')
 def stop():
    data1="STOP"
@@ -73,5 +87,5 @@ def stop():
 
 
 if __name__ == "__main__":
- print "Start"
+ print "Connect on the (local-ip):5010"
  app.run(host='0.0.0.0',port=5010)
